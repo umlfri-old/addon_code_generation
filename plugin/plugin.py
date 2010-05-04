41c4f96c04a466f7b5ec06bbc7ec2210a9a53d0e
junior <kubincanek@yahoo.com>
2010-05-04 13:13:46 +0000

    customized import to handle XML loading exceptions

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -3,7 +3,8 @@ Created on 13.4.2010
 
 @author: kubincam
 '''
-from compiler.ast import For
+from generator.Generator import CGenerator
+from generator.Factory import CLanguageFactory
 
 class pluginMain(object):
     '''
@@ -22,8 +23,20 @@ class pluginMain(object):
             if t.GetGuiId() == 'mItemProject':
                 break;
         sm = t.GetSubmenu();
-        sm.AddMenuItem('test', self.generateCodeFormularCallback, -1, 'Test');
+        sm.AddMenuItem('test', self.generateCodeFormularCallback, -1, 'Generate');
         
-    def generateCodeFormularCallback(self, *widget):
+    def generateCodeFormularCallback(self, widget):
         buffer = [];
-        buffer.append(widget);               
+        buffer.append(widget);
+       
+        print "buffer callback content: ", buffer
+        
+        clan = CLanguageFactory;
+        clan.__Load('../codeTemplate/cplusplus_template.xml')
+        clan.__Load('../codeTemplate/delphi_template.xml')
+        clan.__Load('../codeTemplate/html_documentation.xml')
+        clan.__Load('../codeTemplate/python_temlate.xml')
+        # path where to save generate code
+        gen = CGenerator(clan.GetType(''), 'C:\Users\kubincam\Desktop')
+        
+                     
