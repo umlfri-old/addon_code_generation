c468f9999438806ad775a22b8a810d300fd8e026
junior <junior@d6d51e68-c4c5-48db-96ef-4d935770b557>
2010-05-18 12:02:34 +0000

    generating code support almost done

--- codeGeneration/trunk/plugin/plugin.py
+++ codeGeneration/trunk/plugin/plugin.py
@@ -5,6 +5,10 @@ Created on 13.4.2010
 '''
 from generator.Generator import CGenerator
 from generator.Factory import CLanguageFactory
+from generator.storage import open_storage
+import os
+
+from generator.consts import ROOT_PATH, VERSIONS_PATH, DIAGRAMS_PATH, ELEMENTS_PATH, CONNECTIONS_PATH, DATATYPE_PATH, SOURCECODE_PATH
 
 class pluginMain(object):
     '''
@@ -31,12 +35,17 @@ class pluginMain(object):
        
         print "buffer callback content: ", buffer
         
-        clan = CLanguageFactory;
-        clan.__Load('../codeTemplate/cplusplus_template.xml')
-        clan.__Load('../codeTemplate/delphi_template.xml')
-        clan.__Load('../codeTemplate/html_documentation.xml')
-        clan.__Load('../codeTemplate/python_temlate.xml')
+        # TODO find path
+        self.Storage = open_storage(os.path.join(ROOT_PATH, 'etc', 'uml'))
+        self.CodeEngineering = CLanguageFactory(self.Storage,SOURCECODE_PATH)
+#        clan = CLanguageFactory(storage, path);
+        #clan = CLanguageFactory();
+#        clan.Load('generator/codeTemplate/cplusplus_template.xml')
+#        clan.Load('generator/codeTemplate/delphi_template.xml')
+#        clan.Load('generator/codeTemplate/html_documentation.xml')
+#        clan.Load('generator/codeTemplate/python_temlate.xml')
         # path where to save generate code
-        gen = CGenerator(clan.GetType(''), 'C:\Users\kubincam\Desktop')
+        gen = CGenerator(self.application.GetProject().GetCodeEngineering().GetType(self.cbTargetLanguage.get_active_text()), 'C:\Users\kubincam\Desktop')
+#        gen = CGenerator(clan.GetType(''), 'C:\Users\kubincam\Desktop')
         
                      
