8fb96c1bf617af0eedb0173a0b0f94cbfc861c26
bax <bax007@gmail.com>
2014-05-28 15:45:03 +0000

    Added some functionality. - list in code generation window shows elements in diagram now - cancel button works

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -27,13 +27,39 @@ class pluginMain:
             codeEngineeringMenu = self.__i.gui_manager.main_menu.items["mItemDiagram"].submenu.add_menu_item("mItemCodeEngineering", None, 0, "_Code engineering", True, None)
             codeEngineeringMenu.enabled = False
         self.__i.gui_manager.main_menu.items["mItemDiagram"].submenu.items["mItemCodeEngineering"].add_submenu()
-        self.__i.gui_manager.main_menu.items["mItemDiagram"].submenu.items["mItemCodeEngineering"].submenu.add_menu_item("mItemGenerateCode", self.openCodeGeneratingWindow, 0, "_Generate code", True, None)
+        self.__i.gui_manager.main_menu.items["mItemDiagram"].submenu.items["mItemCodeEngineering"].submenu.add_menu_item("mItemGenerateCode", self.openCodeGenerationWindow, 0, "_Generate code", True, None)
 
-    def openCodeGeneratingWindow(self, widget):
+    """
+    Opens window to set up code generation.
+    @param widget
+    """
+    def openCodeGenerationWindow(self, widget):
         self.__gtkBuilder.add_from_file("share\\addons\\codeGeneration\\plugin\\generateCodeWindow.glade")
-        self.__codeGeneratingWindow = self.__gtkBuilder.get_object("generateSourceCodeWindow")
-        self.__codeGeneratingWindow.set_keep_above(True)
-        self.__codeGeneratingWindow.set_modal(True)
-        self.__codeGeneratingWindow.set_transient_for(None)
-        self.__codeGeneratingWindow.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_DIALOG)
+        if not hasattr(self, '__codeGeneratingWindow') or self.__codeGeneratingWindow is None:
+            self.__codeGeneratingWindow = self.__gtkBuilder.get_object("generateSourceCodeWindow")
+            # Sets window properties.
+            self.__codeGeneratingWindow.set_keep_above(True)
+            self.__codeGeneratingWindow.set_modal(True)
+            self.__codeGeneratingWindow.set_transient_for(None)
+            self.__codeGeneratingWindow.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_DIALOG)
+            # Assign events.
+            self.__gtkBuilder.get_object("cancelButton").connect("clicked",lambda x:self.cancelButtonClicked())
+        # Loads objects from diagram to list.
+        self.loadObjectList()
+        # Shows window.
         self.__codeGeneratingWindow.show_all()
+
+    """
+    Loads data (visual elements in diagram) to object list.
+    """
+    def loadObjectList(self):
+        self.__gtkBuilder.get_object("objectView").get_selection().set_mode(gtk.SELECTION_MULTIPLE)
+        list = self.__gtkBuilder.get_object("objectView").get_model()
+        for element in self.__i.current_diagram.elements:
+            list.append([element.object.name, element.object.type.name])
+
+    """
+    Closes window with no changes.
+    """
+    def cancelButtonClicked(self):
+        self.__codeGeneratingWindow.hide()
